334a58607234efd060237f02fc9cf27c72c9ecc3
lumosmind <mustafakemaltuna@hacettepe.edu.tr>
2019-09-04 00:32:15 +0300

    Scale.RESIZE mode

---
+++ script.js
@@ -0,0 +1,86 @@
+const config = {
+  // chose html5-canvas vs CanvasGL. 
+  type: Phaser.AUTO,
+  //width - height
+  width: 2560,
+  height: 1440,
+  //background color of canvas
+  backgroundColor: 0x225566, //hexadecimal color code
+
+  scale: {
+    autoCenter: Phaser.Scale.CENTER_BOTH, // rescale with aspect ratio
+    // mode: Phaser.Scale.FIT,               // vertical and horizontal center
+    // mode: Phaser.Scale.ENVELOP,               // 
+    mode: Phaser.Scale.RESIZE,               // 
+  },
+  // default scene objects properties
+  scene: {
+    preload: onPreload,     // load assetes before start the game
+    create: onCreate,       // create game object before start game
+    update: onUpdate,       // update game object in game
+  },
+  // physics engine
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { x: 0, y: 0 }, // set gravity
+      debug: true,              // debug mode
+    },
+  },
+};
+
+
+const game = new Phaser.Game(config);
+let cw, ch;
+let sakir, fil, kedi;
+
+function onPreload() {
+  ch = game.canvas.height;
+  cw = game.canvas.width;
+  console.log('cw,ch :', cw, ch);
+  //--------------------------------
+  const baseURL = './assets/images/';
+  this.load.image('background', baseURL + 'arkaplanx.png');
+  // this.load.image('background', baseURL + 'background.jpg');
+  this.load.image('sakir', baseURL + 'sakir.png');
+  this.load.image('fil', baseURL + 'fil.png');
+  this.load.image('kedi', baseURL + 'kedi.png');
+
+}
+
+function onCreate() {
+  this.add.image(game.canvas.width / 2, game.canvas.height / 2, 'background');
+  // sakir = this.add.image(cw * .01, ch - ch * .1, 'sakir').setOrigin();
+  // sakir = this.physics.add.image(0, ch / 2, 'sakir').setAlpha(.4);
+  fil = this.physics.add.image(cw * .01, ch - ch * 0.25, 'fil')
+    .setScale(1)
+
+    .setAlpha(1)
+    .setOrigin(0, 1)
+    .setImmovable();
+
+  const scaleFactor = ch / fil.height / 4;
+  console.log('scaleFactor :', scaleFactor);
+  fil.setScale(scaleFactor);
+
+  sakir = this.physics.add.image(cw * .15, ch - ch * 0.04, 'sakir')
+    .setScale(scaleFactor)
+    .setAlpha(1)
+    .setOrigin(0, 1)
+    .setImmovable();
+
+  kedi = this.physics.add.image(cw * .01, ch - ch * 0.03, 'kedi')
+    .setScale(scaleFactor)
+    .setAlpha(1)
+    .setOrigin(0, 1)
+    .setImmovable();
+
+
+
+
+}
+
+function onUpdate() {
+
+
+}
